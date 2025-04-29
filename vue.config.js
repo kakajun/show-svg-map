0bfde7b2471cd6f40651f07930e1899c5a749508
马军 <majun@xiwicloud.com>
2025-04-29 09:12:31 +0800

    2

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
-  base: './',
+  publicPath: './',
   devServer: {
     proxy: {
       '/mapJson': {
